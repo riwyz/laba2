146ac014665d7be50e776ba24288717c615951d2
riwyz <alehandrageev2@gmail.com>
2023-05-22 15:41:49 +0700

    start

---
+++ laba2/lab2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double total;
+            char oper;
+
+            Console.WriteLine("Введите первое число:");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите оператор:");
+            oper = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число:");
+            b = Convert.ToDouble(Console.ReadLine());
+
+
+            switch (oper)
+            {
+                case '+':
+                    Console.WriteLine(a+b);
+                    break;
+                case '-':
+                    Console.WriteLine(a-b);
+                    break;
+                case '*':
+                    Console.WriteLine(a*b);
+                    break;
+                case '/':
+                    Console.WriteLine(a/b);
+                    break;
+                default:
+                    Console.WriteLine("Неизвестный оператор");
+                    break;
+            }
+        }
+    }
+}
