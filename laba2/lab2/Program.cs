894607c11873d45e5773fea0db7cb06b0f728ed1
riwyz <alehandrageev2@gmail.com>
2023-05-23 18:44:41 +0700

    A

--- laba2/lab2/Program.cs
+++ laba2/lab2/Program.cs
@@ -11,6 +11,7 @@ namespace Program
             char oper;
 
             Console.WriteLine("Введите первое число:");
+            a = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Введите оператор:");
             oper = Convert.ToChar(Console.ReadLine());
