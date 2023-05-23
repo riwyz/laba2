7bdc72d93dbed315bc1b5a9209df61bc256f6e2c
riwyz <alehandrageev2@gmail.com>
2023-05-23 18:42:37 +0700

    begin

--- laba2/lab2/Program.cs
+++ laba2/lab2/Program.cs
@@ -8,32 +8,25 @@ namespace Program
         {
             double a;
             double b;
-            double total;
             char oper;
 
             Console.WriteLine("Введите первое число:");
-            a = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Введите оператор:");
             oper = Convert.ToChar(Console.ReadLine());
 
             Console.WriteLine("Введите второе число:");
-            b = Convert.ToDouble(Console.ReadLine());
 
 
             switch (oper)
             {
                 case '+':
-                    Console.WriteLine(a+b);
                     break;
                 case '-':
-                    Console.WriteLine(a-b);
                     break;
                 case '*':
-                    Console.WriteLine(a*b);
                     break;
                 case '/':
-                    Console.WriteLine(a/b);
                     break;
                 default:
                     Console.WriteLine("Неизвестный оператор");
