d59b6ebcff1d46d6bacccd48620eecd32c64afbd
riwyz <alehandrageev2@gmail.com>
2023-05-23 18:52:54 +0700

    B

--- laba2/lab2/Program.cs
+++ laba2/lab2/Program.cs
@@ -16,7 +16,7 @@ namespace Program
             oper = Convert.ToChar(Console.ReadLine());
 
             Console.WriteLine("Введите второе число:");
-
+            b = Convert.ToDouble(Console.ReadLine());
 
             switch (oper)
             {
